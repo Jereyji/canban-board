ba5b0f2e1b21c27203d9f4bd68a8be15c1dc0dd4
Pipe Bombadil <pipebomb@in-e2.nsk.21-school.ru>
2024-04-15 19:50:26 +0700

    formate handler.go

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,31 +3,35 @@ package handler
 import "github.com/gin-gonic/gin"
 
 type Handler struct {
-
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.Default()
 
-	auth := router.Group("/auth"); {
+	auth := router.Group("/auth")
+	{
 		auth.POST("/sign-up", h.signUp)
 		auth.POST("/sign-in", h.signIn)
 	}
 
-	api := router.Group("/api"); {
-		boards := api.Group("/boards"); {
+	api := router.Group("/api")
+	{
+		boards := api.Group("/boards")
+		{
 			boards.POST("/", h.createBoard)
 			boards.GET("/", h.getAllBoards)
 			boards.GET("/:id", h.getBoardById)
 			boards.PUT("/:id", h.updateBoard)
 			boards.DELETE("/:id", h.deleteBoard)
 
-			cards := boards.Group(":id/cards"); {
+			cards := boards.Group(":id/cards")
+			{
 				cards.POST("/", h.createCard)
 				cards.GET("/", h.getAllCards)
 			}
 		}
-		cards := boards.Group("/cards"); {
+		cards := boards.Group("/cards")
+		{
 			cards.GET("/:id", h.getCardById)
 			cards.PUT("/:id", h.updateCard)
 			cards.DELETE("/:id", h.deleteCard)
